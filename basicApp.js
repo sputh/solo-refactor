742c7a954d56c5e805a68d06d6a9ca9ee71857f7
sputh <samantha.puth@gmail.com>
2014-08-25 17:15:54 -0700

    Began integrating npr

--- basicApp.js
+++ basicApp.js
@@ -1,8 +1,44 @@
+var $followings = $('#followings');
+
 // Get's Reddit's JSON
 
-$.getJSON('http://reddit.com/.json?jsonp=?', function(data) {
-	// $.each(data.children, function(i, item) {
+var reddit = function(url) {
+	$.getJSON(url,
+	  function (data){
+	  	$.each(data.data.children,
+      function (i, post) {
+        $('#followings').append( '<br>' + '<a href=post.data.url>' + post.data.title +'</a>');
+        $('#followings').append( '<br>' + post.data.url );
+        $('#followings').append( '<br>' + post.data.permalink );
+        $('#followings').append( '<br>' + post.data.ups );
+        $('#followings').append( '<br>' + post.data.downs );
+        $('#followings').append( '<hr>' );
+	  });
+	});
+}
 
-	// })
-	console.log(data);
-})
+// reddit('http://www.reddit.com/.json?jsonp=?');
+
+// $('#addSource').on('click', function(){
+// 	alert('you've clicked it');
+// })
+// Get's NPR's RSS Feed
+$.getJSON('http://api.npr.org/query?id=1001&apiKey=MDE2NDAyNjQ0MDE0MDkwMTA3NjdiNDRlYQ001&output=json', 
+	function (data) {
+		$.each(data.list, 
+			function(i, post) {
+				// $('#followings').append( '<br>' + post.title)
+				console.log("post", post.$text);
+			})
+		console.log('data', data)
+	}
+)      
+		// $.each(data.item,
+		// 	function(i, post) {
+		// 		console.log("i", i);
+		// 		console.log('post', post);
+		// 	}
+		// 	// $('#followings').append( '<br>' + data.title.$text);
+		// 	// $('#followings').append('<hr>');
+		// );
+		// console.log('data', data);}
